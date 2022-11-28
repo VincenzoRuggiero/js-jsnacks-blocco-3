f11af18d3050bed174ef24055e2262ff35837980
VincenzoRuggiero <vincenzoru@live.it>
2022-11-28 17:29:17 +0100

    Update

--- snack2/snack2.js
+++ snack2/snack2.js
@@ -1,15 +1,10 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
 const numsArr = [];
+let sum = 0;
 
-let i = 0;
-while (i < 50) {
-  let userNums1 = parseInt(prompt("Inserisci un numero"));
-  let userNums2 = parseInt(prompt("Inserisci un numero"));
-
-  let sum = userNums1 + userNums2;
-
-  console.log(sum);
-
-  numsArr.push(sum);
+while (sum < 50) {
+  let userNums = parseInt(prompt("Inserisci un numero"));
+  sum += userNums;
+  numsArr.push(userNums);
 }
